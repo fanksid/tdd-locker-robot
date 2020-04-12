cf910338422826ca82172a555789c99ae95e8590
Kong Fanyue <fykong@thoughtworks.com>
2020-04-12 11:24:31 +0800

    refactor: use else throw to reduce if branch

--- src/main/java/cn/xpbootcamp/gilded_rose/LockerRobot.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/LockerRobot.java
@@ -3,7 +3,6 @@ package cn.xpbootcamp.gilded_rose;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 public abstract class LockerRobot {
     protected List<Locker> lockers;
@@ -15,27 +14,21 @@ public abstract class LockerRobot {
     abstract Comparator<Locker> sort();
 
     public LockerTicket save(Bag bag) {
-        Optional<Locker> locker = lockers.stream()
+        Locker locker = lockers.stream()
                 .sorted(sort())
                 .filter(Locker::hasSpace)
-                .findFirst();
+                .findFirst()
+                .orElseThrow(CapacityFullException::new);
 
-        if (locker.isPresent()) {
-            return locker.get().save(bag);
-        } else {
-            throw new CapacityFullException();
-        }
+        return locker.save(bag);
     }
 
     public Bag pick(LockerTicket ticket) {
-        Optional<Locker> locker = lockers.stream()
+        Locker locker = lockers.stream()
                 .filter(t -> t.isIn(ticket))
-                .findFirst();
+                .findFirst()
+                .orElseThrow(InvalidTicketException::new);
 
-        if (locker.isPresent()) {
-            return locker.get().pick(ticket);
-        } else {
-            throw new InvalidTicketException();
-        }
+        return locker.pick(ticket);
     }
 }
