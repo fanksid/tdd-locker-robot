1896b8af50780f5aecaa43b296205959c15b8218
Kong Fanyue <fykong@thoughtworks.com>
2020-04-15 22:33:21 +0800

    feat: add super robot can save and pick bag sequentially when has two same rate lockers

--- src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
@@ -16,4 +16,19 @@ public class SuperLockerRobotTest {
 
         Assertions.assertSame(bag, picked);
     }
+
+    @Test
+    void should_return_ticket_and_pick_bag_from_first_locker_given_robot_has_two_same_space_rate_lockers_when_save() {
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(2);
+        SuperLockerRobot robot = new SuperLockerRobot(locker1, locker2);
+        Bag bag = new Bag();
+
+        LockerTicket ticket = robot.save(bag);
+        Assertions.assertSame(bag, locker1.pick(ticket));
+
+        ticket = robot.save(bag);
+        Assertions.assertSame(bag, robot.pick(ticket));
+    }
+
 }
